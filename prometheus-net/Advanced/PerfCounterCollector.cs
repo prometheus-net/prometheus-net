5146eb0d0d22dc6bf9c2ec11f40d2339878815a1
David Chapman <David.Chapman@mavensecurities.com>
2015-11-26 10:57:16 +0000

    Allow specifying alternative default collectors Implement non-perf counter based metric collection

--- prometheus-net/Advanced/PerfCounterCollector.cs
+++ prometheus-net/Advanced/PerfCounterCollector.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Prometheus.Advanced.DataContracts;
 
 namespace Prometheus.Advanced
 {
     /// <summary>
     /// Collects metrics on standard Performance Counters
     /// </summary>
-    public class PerfCounterCollector : ICollector
+    public class PerfCounterCollector : IOnDemandCollector
     {
         private const string MemCat = ".NET CLR Memory";
         private const string ProcCat = "Process";
@@ -44,7 +43,6 @@ namespace Prometheus.Advanced
 
         public PerfCounterCollector()
         {
-            Name = "performanceCounters";
             Process currentProcess = Process.GetCurrentProcess();
             _instanceName = currentProcess.ProcessName;
             if (IsLinux())
@@ -54,23 +52,8 @@ namespace Prometheus.Advanced
             }
         }
 
-        public void RegisterStandardPerfCounters()
+        private void RegisterPerfCounter(string category, string name)
         {
-            for (int i = 0; i < StandardPerfCounters.Length; i += 2)
-            {
-                var category = StandardPerfCounters[i];
-                var name = StandardPerfCounters[i + 1];
-
-                RegisterPerfCounter(category, name);
-            }
-
-            _perfErrors = Metrics.CreateCounter("performance_counter_errors_total",
-                "Total number of errors that occured during performance counter collections");
-        }
-
-        public void RegisterPerfCounter(string category, string name)
-        {
-            //Gauge.Child labelledMetric = Metrics.CreateGauge(GetName(category, name), GetHelp(name), "process").Labels(_processName);
             Gauge gauge = Metrics.CreateGauge(GetName(category, name), GetHelp(name));
             _collectors.Add(Tuple.Create(gauge, new PerformanceCounter(category, name, _instanceName)));
         }
@@ -90,9 +73,22 @@ namespace Prometheus.Advanced
             return name.Replace("%", "pct").Replace(" ", "_").Replace(".", "dot").ToLowerInvariant();
         }
 
-        public MetricFamily Collect()
+        public void RegisterMetrics()
         {
-            //update existing counters during a collect call
+            for (int i = 0; i < StandardPerfCounters.Length; i += 2)
+            {
+                var category = StandardPerfCounters[i];
+                var name = StandardPerfCounters[i + 1];
+
+                RegisterPerfCounter(category, name);
+            }
+
+            _perfErrors = Metrics.CreateCounter("performance_counter_errors_total",
+                "Total number of errors that occured during performance counter collections");
+        }
+
+        public void UpdateMetrics()
+        {
             foreach (var collector in _collectors)
             {
                 try
@@ -104,16 +100,6 @@ namespace Prometheus.Advanced
                     _perfErrors.Inc();
                 }
             }
-
-            //the way this collector is registered on DefaultCollectorRegistry ensures that this is the very first Collector collected
-            //so by the time the collectors registered in the _collectors list are collected they will all be up-to-date
-            //this works but a bit hacky...
-
-            //don't return anything - this will be dropped by the registry...
-            return null;
         }
-
-        public string Name { get; private set; }
-        public string[] LabelNames { get {return new string[0];} }
     }
 }
