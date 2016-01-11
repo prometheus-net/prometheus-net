824d3a7d21420b19f364770f13e59c426eafbd8e
andrasm <andrasgit@gmail.com>
2016-01-11 21:29:28 +0100

    bumped version, switched to msbuild 14

--- deploy_nuget.bat
+++ deploy_nuget.bat
@@ -1,3 +1,4 @@
 ::todo: use FAKE 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild prometheus-net.sln /p:Configuration=Release
+::C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild prometheus-net.sln /p:Configuration=Release
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" prometheus-net.sln /p:Configuration=Release
 nuget pack prometheus-net/prometheus-net.csproj -Prop Configuration=Release
