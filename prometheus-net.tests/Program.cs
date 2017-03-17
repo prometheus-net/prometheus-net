d9c41146f913978dabaa1651d71d4a701dbbaf92
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2017-03-17 00:10:51 -0400

    Configure NUnitLite runner

---
+++ prometheus-net.tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Common;
+using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace Prometheus.Tests
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+              .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+        }
+    }
+}
