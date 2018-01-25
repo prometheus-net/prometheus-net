7f27d0c6636af1fc6f4e536547ebbfd76150b793
Sander Saares <saares@axinom.com>
2018-01-25 07:21:17 +0200

    Added benchmark app for label benchmarking

---
+++ Benchmark.NetFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    public sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<LabelBenchmarks>();
+        }
+    }
+}
