380e4abfe8ff166f191db54205fd83119f4a4cac
Sander Saares <saares@axinom.com>
2018-09-25 10:03:32 +0300

    2.1.3

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 // This is the real version number, used in NuGet packages and for display purposes.
-[assembly: AssemblyFileVersion("2.1.2")]
+[assembly: AssemblyFileVersion("2.1.3")]
 
 // Only use major version here, with others kept at zero, for correct assembly binding logic.
 [assembly: AssemblyVersion("2.0.0")]
