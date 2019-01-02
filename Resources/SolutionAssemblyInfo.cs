b200f81f690b659c6830a16b61dbd8f7e2d7918c
Sander Saares <saares@axinom.com>
2019-01-02 12:21:00 +0200

    Bump version to 3.0.0

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.CompilerServices;
 
 // This is the real version number, used in NuGet packages and for display purposes.
-[assembly: AssemblyFileVersion("2.1.3")]
+[assembly: AssemblyFileVersion("3.0.0")]
 
 // Only use major version here, with others kept at zero, for correct assembly binding logic.
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
 
 [assembly: InternalsVisibleTo("Tests.NetFramework")]
 [assembly: InternalsVisibleTo("Tests.NetCore")]
