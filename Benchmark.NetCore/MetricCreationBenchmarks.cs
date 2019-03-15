8f73ae74f5c6dc06e1996d29309e47d1b4e6ccaf
Sander Saares <saares@axinom.com>
2019-03-15 18:14:07 +0800

    Benchmarks about duplicates, too

--- Benchmark.NetCore/MetricCreationBenchmarks.cs
+++ Benchmark.NetCore/MetricCreationBenchmarks.cs
@@ -15,6 +15,11 @@ namespace Benchmark.NetCore
         /// </summary>
         private const int _metricCount = 10000;
 
+        /// <summary>
+        /// Some benchmarks try to register metrics that already exist.
+        /// </summary>
+        private const int _duplicateCount = 5;
+
         private const string _help = "arbitrary help message for metric, not relevant for benchmarking";
 
         private static readonly string[] _metricNames;
@@ -74,6 +79,126 @@ namespace Benchmark.NetCore
         {
             for (var i = 0; i < _metricCount; i++)
                 _factory.CreateHistogram(_metricNames[i], _help, new HistogramConfiguration
+                {
+                    LabelNames = _labelNames
+                }).Observe(123);
+        }
+
+        [Benchmark]
+        public void Counter_Many_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                for (var i = 0; i < _metricCount; i++)
+                    _factory.CreateCounter(_metricNames[i], _help, new CounterConfiguration
+                    {
+                        LabelNames = _labelNames
+                    }).Inc();
+        }
+
+        [Benchmark]
+        public void Gauge_Many_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                for (var i = 0; i < _metricCount; i++)
+                    _factory.CreateGauge(_metricNames[i], _help, new GaugeConfiguration
+                    {
+                        LabelNames = _labelNames
+                    }).Inc();
+        }
+
+        [Benchmark]
+        public void Summary_Many_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                for (var i = 0; i < _metricCount; i++)
+                    _factory.CreateSummary(_metricNames[i], _help, new SummaryConfiguration
+                    {
+                        LabelNames = _labelNames
+                    }).Observe(123);
+        }
+
+        [Benchmark]
+        public void Histogram_Many_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                for (var i = 0; i < _metricCount; i++)
+                    _factory.CreateHistogram(_metricNames[i], _help, new HistogramConfiguration
+                    {
+                        LabelNames = _labelNames
+                    }).Observe(123);
+        }
+
+        [Benchmark]
+        public void Counter()
+        {
+            _factory.CreateCounter(_metricNames[0], _help, new CounterConfiguration
+            {
+                LabelNames = _labelNames
+            }).Inc();
+        }
+
+        [Benchmark]
+        public void Gauge()
+        {
+            _factory.CreateGauge(_metricNames[0], _help, new GaugeConfiguration
+            {
+                LabelNames = _labelNames
+            }).Inc();
+        }
+
+        [Benchmark]
+        public void Summary()
+        {
+            _factory.CreateSummary(_metricNames[0], _help, new SummaryConfiguration
+            {
+                LabelNames = _labelNames
+            }).Observe(123);
+        }
+
+        [Benchmark]
+        public void Histogram()
+        {
+            _factory.CreateHistogram(_metricNames[0], _help, new HistogramConfiguration
+            {
+                LabelNames = _labelNames
+            }).Observe(123);
+        }
+
+        [Benchmark]
+        public void Counter_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                _factory.CreateCounter(_metricNames[0], _help, new CounterConfiguration
+                {
+                    LabelNames = _labelNames
+                }).Inc();
+        }
+
+        [Benchmark]
+        public void Gauge_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                _factory.CreateGauge(_metricNames[0], _help, new GaugeConfiguration
+                {
+                    LabelNames = _labelNames
+                }).Inc();
+        }
+
+        [Benchmark]
+        public void Summary_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                _factory.CreateSummary(_metricNames[0], _help, new SummaryConfiguration
+                {
+                    LabelNames = _labelNames
+                }).Observe(123);
+        }
+
+        [Benchmark]
+        public void Histogram_Duplicates()
+        {
+            for (var dupe = 0; dupe < _duplicateCount; dupe++)
+                _factory.CreateHistogram(_metricNames[0], _help, new HistogramConfiguration
                 {
                     LabelNames = _labelNames
                 }).Observe(123);
