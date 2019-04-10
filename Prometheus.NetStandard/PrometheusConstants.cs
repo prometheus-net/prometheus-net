cd3cd18432eb319713195fdc67c134fbdbdb87e6
Sander Saares <saares@axinom.com>
2019-04-10 13:53:42 +0800

    3.1.1 Added missing UTF-8 charset to Content-Type header, so non-ASCII characters are interpreted correctly by browsers.
    Ref #145

--- Prometheus.NetStandard/PrometheusConstants.cs
+++ Prometheus.NetStandard/PrometheusConstants.cs
@@ -4,7 +4,7 @@ namespace Prometheus
 {
     public static class PrometheusConstants
     {
-        public const string ExporterContentType = "text/plain; version=0.0.4";
+        public const string ExporterContentType = "text/plain; version=0.0.4; charset=utf-8";
 
         // Use UTF-8 encoding, but provide the flag to ensure the Unicode Byte Order Mark is never
         // pre-pended to the output stream.
