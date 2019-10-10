8e840df89b6a04cceb3373ae00f4dedf0a032e7e
rwkarg <rwkarg@gmail.com>
2019-10-09 18:06:33 -0700

    Adding port filtering ability for Metric Server

--- Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
+++ Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
@@ -8,11 +8,20 @@ namespace Prometheus
         /// Starts a Prometheus metrics exporter. The default URL is /metrics, which is a Prometheus convention.
         /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
         /// </summary>
-        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, string? url = "/metrics", CollectorRegistry? registry = null)
+        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, string? url = "/metrics", CollectorRegistry? registry = null, int? port)
         {
             // If there is a URL to map, map it and re-enter without the URL.
             if (url != null)
-                return builder.Map(url, builder2 => builder2.UseMetricServer(null, registry));
+            {
+                if(port.HasValue)
+                {
+                    return builder.MapWhen(
+                        c => c.Connection.LocalPort == port,
+                        b0 => b0.Map(path, b1 => b1.UseMetricServer(null, registry)));
+                }
+                else
+                    return builder.Map(url, builder2 => builder2.UseMetricServer(null, registry));
+            }
 
             return builder.UseMiddleware<MetricServerMiddleware>(new MetricServerMiddleware.Settings
             {
