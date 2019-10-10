3792bd98d6b4849e5fa7fb7b76b4a03d46b3ef53
Karg <rkarg@blizzard.com>
2019-10-10 11:45:18 -0700

    Cleaning up implementation

--- Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
+++ Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 
 namespace Prometheus
 {
     public static class MetricServerMiddlewareExtensions
     {
+        /// <summary>
+        /// Starts a Prometheus metrics exporter on a specific port.
+        /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
+        /// </summary>
+        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, PathAndPort pathAndPort, CollectorRegistry? registry = null)
+        {
+            return builder
+                .Map(pathAndPort.Path, b => b.MapWhen(PortMatches(), b1 => b1.InternalUseMiddleware(registry)));
+
+            Func<HttpContext, bool> PortMatches()
+            {
+                return c => c.Connection.LocalPort == pathAndPort.Port;
+            }
+        }
+
         /// <summary>
         /// Starts a Prometheus metrics exporter. The default URL is /metrics, which is a Prometheus convention.
         /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
@@ -12,24 +29,17 @@ namespace Prometheus
         {
             // If there is a URL to map, map it and re-enter without the URL.
             if (url != null)
-                return builder.Map(url, builder2 => builder2.UseMetricServer((string?)null, registry));
+                return builder.Map(url, b => b.InternalUseMiddleware(registry));
+            else
+                return builder.InternalUseMiddleware(registry);
+        }
 
+        private static IApplicationBuilder InternalUseMiddleware(this IApplicationBuilder builder, CollectorRegistry? registry = null)
+        {
             return builder.UseMiddleware<MetricServerMiddleware>(new MetricServerMiddleware.Settings
             {
                 Registry = registry
             });
-        }
-
-
-        /// <summary>
-        /// Starts a Prometheus metrics exporter on a specific port.
-        /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
-        /// </summary>
-        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, PathAndPort pathAndPort, CollectorRegistry? registry = null)
-        {
-            return builder.MapWhen(
-                c => c.Connection.LocalPort == pathAndPort.Port,
-                b0 => b0.Map(pathAndPort.Path, b1 => b1.UseMetricServer((string?)null, registry)));
         }
 
         public class PathAndPort
@@ -40,13 +50,19 @@ namespace Prometheus
                 Port = port;
             }
 
+            /// <summary>
+            /// Creates a <see cref="PathAndPort"/> with the default URL <c>/metrics</c>, which is a Prometheus convention.
+            /// </summary>
+            /// <param name="port"></param>
+            /// <returns></returns>
             public static PathAndPort WithDefaultPath(int port)
             {
                 return new PathAndPort("/metrics", port);
             }
 
-            public string Path { get; set; }
-            public int Port { get; set; }
+            public string Path { get; }
+            public int Port { get; }
         }
+
     }
 }
