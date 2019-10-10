1867763e60cb9002d110e6fc38769a03f780af7d
Karg <rkarg@blizzard.com>
2019-10-10 10:34:59 -0700

    Creating separate method for PathAndPort specification

--- Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
+++ Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
@@ -8,25 +8,45 @@ namespace Prometheus
         /// Starts a Prometheus metrics exporter. The default URL is /metrics, which is a Prometheus convention.
         /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
         /// </summary>
-        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, string? url = "/metrics", CollectorRegistry? registry = null, int? port)
+        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, string? url = "/metrics", CollectorRegistry? registry = null)
         {
             // If there is a URL to map, map it and re-enter without the URL.
             if (url != null)
-            {
-                if(port.HasValue)
-                {
-                    return builder.MapWhen(
-                        c => c.Connection.LocalPort == port,
-                        b0 => b0.Map(path, b1 => b1.UseMetricServer(null, registry)));
-                }
-                else
-                    return builder.Map(url, builder2 => builder2.UseMetricServer(null, registry));
-            }
+                return builder.Map(url, builder2 => builder2.UseMetricServer((string?)null, registry));
 
             return builder.UseMiddleware<MetricServerMiddleware>(new MetricServerMiddleware.Settings
             {
                 Registry = registry
             });
+        }
+
+
+        /// <summary>
+        /// Starts a Prometheus metrics exporter on a specific port.
+        /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
+        /// </summary>
+        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, PathAndPort pathAndPort, CollectorRegistry? registry = null)
+        {
+            return builder.MapWhen(
+                c => c.Connection.LocalPort == pathAndPort.Port,
+                b0 => b0.Map(pathAndPort.Path, b1 => b1.UseMetricServer((string?)null, registry)));
+        }
+
+        public class PathAndPort
+        {
+            public PathAndPort(string path, int port)
+            {
+                Path = path;
+                Port = port;
+            }
+
+            public static PathAndPort WithDefaultPath(int port)
+            {
+                return new PathAndPort("/metrics", port);
+            }
+
+            public string Path { get; set; }
+            public int Port { get; set; }
         }
     }
 }
