271c8c0d4f3bf4a79408bc39fe0d61994726d46f
rwkarg <rwkarg@gmail.com>
2019-10-18 04:44:17 -0700

    Allowing specifying a port filter for hosting ASP.NET Core metric server (#160)

--- Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
+++ Prometheus.AspNetCore/MetricServerMiddlewareExtensions.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 
 namespace Prometheus
 {
     public static class MetricServerMiddlewareExtensions
     {
+        /// <summary>
+        /// Starts a Prometheus metrics exporter on a specific port.
+        /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
+        /// </summary>
+        public static IApplicationBuilder UseMetricServer(this IApplicationBuilder builder, int port, string? url = "/metrics", CollectorRegistry? registry = null)
+        {
+            return builder
+                .Map(url, b => b.MapWhen(PortMatches(), b1 => b1.InternalUseMiddleware(registry)));
+
+            Func<HttpContext, bool> PortMatches()
+            {
+                return c => c.Connection.LocalPort == port;
+            }
+        }
+
         /// <summary>
         /// Starts a Prometheus metrics exporter. The default URL is /metrics, which is a Prometheus convention.
         /// Use static methods on the <see cref="Metrics"/> class to create your metrics.
@@ -12,8 +29,13 @@ namespace Prometheus
         {
             // If there is a URL to map, map it and re-enter without the URL.
             if (url != null)
-                return builder.Map(url, builder2 => builder2.UseMetricServer(null, registry));
+                return builder.Map(url, b => b.InternalUseMiddleware(registry));
+            else
+                return builder.InternalUseMiddleware(registry);
+        }
 
+        private static IApplicationBuilder InternalUseMiddleware(this IApplicationBuilder builder, CollectorRegistry? registry = null)
+        {
             return builder.UseMiddleware<MetricServerMiddleware>(new MetricServerMiddleware.Settings
             {
                 Registry = registry
