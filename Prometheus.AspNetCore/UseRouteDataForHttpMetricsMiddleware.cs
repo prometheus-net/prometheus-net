d9350bcdbc841b94245e405eefc7547fecb3f9ab
Sander Saares <sander@saares.eu>
2019-12-19 13:43:16 +0200

    More XMLdoc comments

--- Prometheus.AspNetCore/UseRouteDataForHttpMetricsMiddleware.cs
+++ Prometheus.AspNetCore/UseRouteDataForHttpMetricsMiddleware.cs
@@ -6,7 +6,8 @@ namespace Prometheus
 {
     /// <summary>
     /// Uses ASP.NET Core MVC routing data from a specific point in the ASP.NET Core pipeline.
-    /// Without this, the routing data is used from the point where the HTTP metrics middleware is installed.
+    /// Without this, the routing data is used from the point where the HTTP metrics middleware is installed,
+    /// after request processing, which might be too late if something changed the routing data during processing.
     /// </summary>
     /// <remarks>
     /// This is useful if some steps overwrite the route data and you wish to record the original data in metrics.
