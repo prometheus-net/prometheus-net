eff2a83379b574a8efb4656fdd789c8dc65ac5e3
Sander Saares <sander@saares.eu>
2019-12-22 16:29:03 +0000

    HttpListener to handle concurrent requests
    The HttpListener based MetricServer can now handle more than one concurrent request.

--- Prometheus.NetStandard/MetricServer.cs
+++ Prometheus.NetStandard/MetricServer.cs
@@ -31,7 +31,7 @@ namespace Prometheus
             _httpListener.Start();
 
             // Kick off the actual processing to a new thread and return a Task for the processing thread.
-            return Task.Factory.StartNew(async delegate
+            return Task.Factory.StartNew(delegate
             {
                 try
                 {
@@ -41,60 +41,70 @@ namespace Prometheus
                         var getContext = _httpListener.GetContextAsync();
                         getContext.Wait(cancel);
                         var context = getContext.Result;
-                        var request = context.Request;
-                        var response = context.Response;
 
-                        try
-                        {
-                            try
-                            {
-                                // We first touch the response.OutputStream only in the callback because touching
-                                // it means we can no longer send headers (the status code).
-                                var serializer = new TextSerializer(delegate
-                                {
-                                    response.ContentType = PrometheusConstants.ExporterContentType;
-                                    response.StatusCode = 200;
-                                    return response.OutputStream;
-                                });
+                        // Kick the request off to a background thread for processing.
+                        _ = Task.Factory.StartNew(async delegate
+                          {
+                              var request = context.Request;
+                              var response = context.Response;
 
-                                await _registry.CollectAndSerializeAsync(serializer, cancel);
-                                response.OutputStream.Dispose();
-                            }
-                            catch (ScrapeFailedException ex)
-                            {
-                                // This can only happen before anything is written to the stream, so it
-                                // should still be safe to update the status code and report an error.
-                                response.StatusCode = 503;
+                              try
+                              {
+                                  try
+                                  {
+                                      // We first touch the response.OutputStream only in the callback because touching
+                                      // it means we can no longer send headers (the status code).
+                                      var serializer = new TextSerializer(delegate
+                                        {
+                                            response.ContentType = PrometheusConstants.ExporterContentType;
+                                            response.StatusCode = 200;
+                                            return response.OutputStream;
+                                        });
 
-                                if (!string.IsNullOrWhiteSpace(ex.Message))
-                                {
-                                    using (var writer = new StreamWriter(response.OutputStream))
-                                        writer.Write(ex.Message);
-                                }
-                            }
-                        }
-                        catch (Exception ex) when (!(ex is OperationCanceledException))
-                        {
-                            Trace.WriteLine(string.Format("Error in MetricsServer: {0}", ex));
+                                      await _registry.CollectAndSerializeAsync(serializer, cancel);
+                                      response.OutputStream.Dispose();
+                                  }
+                                  catch (ScrapeFailedException ex)
+                                  {
+                                      // This can only happen before anything is written to the stream, so it
+                                      // should still be safe to update the status code and report an error.
+                                      response.StatusCode = 503;
 
-                            try
-                            {
-                                response.StatusCode = 500;
-                            }
-                            catch
-                            {
-                                // Might be too late in request processing to set response code, so just ignore.
-                            }
-                        }
-                        finally
-                        {
-                            response.Close();
-                        }
+                                      if (!string.IsNullOrWhiteSpace(ex.Message))
+                                      {
+                                          using (var writer = new StreamWriter(response.OutputStream))
+                                              writer.Write(ex.Message);
+                                      }
+                                  }
+                              }
+                              catch (Exception ex) when (!(ex is OperationCanceledException))
+                              {
+                                  if (!_httpListener.IsListening)
+                                      return; // We were shut down.
+
+                                  Trace.WriteLine(string.Format("Error in MetricsServer: {0}", ex));
+
+                                  try
+                                  {
+                                      response.StatusCode = 500;
+                                  }
+                                  catch
+                                  {
+                                      // Might be too late in request processing to set response code, so just ignore.
+                                  }
+                              }
+                              finally
+                              {
+                                  response.Close();
+                              }
+                          }, TaskCreationOptions.LongRunning);
+
                     }
                 }
                 finally
                 {
                     _httpListener.Stop();
+                    // This should prevent any currently processed requests from finishing.
                     _httpListener.Close();
                 }
             }, TaskCreationOptions.LongRunning);
