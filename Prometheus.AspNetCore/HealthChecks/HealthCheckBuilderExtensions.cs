050deabcaa727434bf6f878a3fe5535136c1e14c
fahri.goktuna <fahri.goktuna@turkcell.com.tr>
2020-04-05 13:47:47 +0300

    AspNetCore HealthCheckPublisher Added with IServiceCollection extension

---
+++ Prometheus.AspNetCore/HealthChecks/HealthCheckBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Prometheus.HealthChecks
+{
+    public static class HealthCheckBuilderExtensions
+    {
+        public static IHealthChecksBuilder ForwardToPrometheus(this IHealthChecksBuilder builder)
+        {
+            builder.Services.AddSingleton<IHealthCheckPublisher, HealthCheckPublisher>();
+
+            return builder;
+        }
+    }
+}
