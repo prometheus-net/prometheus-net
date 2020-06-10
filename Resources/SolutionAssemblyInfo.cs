a97cb14bfc2a569cad5ab9b4a4954c22e9b507e7
Sander Saares <sander@saares.eu>
2020-06-10 09:36:27 +0300

    3.6.0 for new features

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 // This is the real version number, used in NuGet packages and for display purposes.
-[assembly: AssemblyFileVersion("3.5.0")]
+[assembly: AssemblyFileVersion("3.6.0")]
 
 // Only use major version here, with others kept at zero, for correct assembly binding logic.
 [assembly: AssemblyVersion("3.0.0")]
