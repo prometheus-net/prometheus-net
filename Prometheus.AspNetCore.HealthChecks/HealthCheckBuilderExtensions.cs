c2edd32c35dd680706bf02fb8da786e848aa8bcd
Sander Saares <sander@saares.eu>
2020-06-10 09:18:39 +0300

    Pre-merge adjustments to health checks functionality

---
+++ Prometheus.AspNetCore.HealthChecks/HealthCheckBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Prometheus
+{
+    public static class HealthCheckBuilderExtensions
+    {
+        public static IHealthChecksBuilder ForwardToPrometheus(this IHealthChecksBuilder builder, PrometheusHealthCheckPublisherOptions? options = null)
+        {
+            builder.Services.AddSingleton<IHealthCheckPublisher, PrometheusHealthCheckPublisher>(provider => new PrometheusHealthCheckPublisher(options));
+
+            return builder;
+        }
+    }
+}
