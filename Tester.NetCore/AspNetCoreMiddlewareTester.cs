1867e211835879d208e8c79c8d87d5338cf38291
Sander Saares <sander@saares.eu>
2020-07-06 09:05:39 +0300

    ASP.NET Core 3 tester only under NetCore
    For simplicity of maintenance. ASP.NET Core 2 still works but we don't have a tester for it anymore (famous last words?)

---
+++ Tester.NetCore/AspNetCoreMiddlewareTester.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Prometheus;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace tester
+{
+    /// <summary>
+    /// This targets ASP.NET Core 3.
+    /// </summary>
+    internal class AspNetCoreMiddlewareTester : Tester
+    {
+        // Sinaled when it is time for the web server to stop.
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+        private Task _webserverTask;
+
+        public override void OnStart()
+        {
+            _webserverTask =
+                WebHost.CreateDefaultBuilder()
+                .UseUrls($"http://localhost:{TesterConstants.TesterPort}")
+                .ConfigureServices(services =>
+                {
+                })
+                .Configure(app =>
+                {
+                    app.UseMetricServer();
+
+                    app.UseRouting();
+                    app.UseHttpMetrics();
+                })
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .Build()
+                .RunAsync(_cts.Token);
+        }
+
+        public override void OnTimeToObserveMetrics()
+        {
+            // Every time we observe metrics, we also asynchronously perform a dummy request for test data.
+            StartDummyRequest();
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create($"http://localhost:{TesterConstants.TesterPort}/metrics");
+            httpRequest.Method = "GET";
+
+            using (var httpResponse = (HttpWebResponse)httpRequest.GetResponse())
+            {
+                var text = new StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+
+        private void StartDummyRequest()
+        {
+            Task.Run(delegate
+            {
+                var httpRequest = (HttpWebRequest)WebRequest.Create($"http://localhost:{TesterConstants.TesterPort}/api/Dummy");
+                httpRequest.Method = "GET";
+
+                httpRequest.GetResponse().Dispose();
+            });
+        }
+
+        public override void OnEnd()
+        {
+            _cts.Cancel();
+
+            try
+            {
+                _webserverTask.GetAwaiter().GetResult();
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+
+        public override IMetricServer InitializeMetricServer() => null;
+    }
+}
--- Tester.NetFramework/AspNetCoreMiddlewareTester.cs
+++ Tester.NetFramework/AspNetCoreMiddlewareTester.cs
@@ -1,83 +1,13 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Hosting;
-using Prometheus;
+﻿using Prometheus;
 using System;
-using System.IO;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 
 namespace tester
 {
-    internal class AspNetCoreMiddlewareTester : Tester
+    sealed class AspNetCoreMiddlewareTester : Tester
     {
-        // Sinaled when it is time for the web server to stop.
-        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
-
-        private Task _webserverTask;
-
-        public override void OnStart()
+        public override IMetricServer InitializeMetricServer()
         {
-            _webserverTask =
-                WebHost.CreateDefaultBuilder()
-                .UseUrls($"http://localhost:{TesterConstants.TesterPort}")
-                .ConfigureServices(services =>
-                {
-                    services.AddMvc();
-                })
-                .Configure(app =>
-                {
-                    app.UseMetricServer();
-                    app.UseHttpMetrics();
-                    app.UseMvc();
-                })
-                .ConfigureLogging(logging => logging.ClearProviders())
-                .Build()
-                .RunAsync(_cts.Token);
+            throw new NotImplementedException("ASP.NET Core tester is only available under .NET Core");
         }
-
-        public override void OnTimeToObserveMetrics()
-        {
-            // Every time we observe metrics, we also asynchronously perform a dummy request for test data.
-            StartDummyRequest();
-
-            var httpRequest = (HttpWebRequest)WebRequest.Create($"http://localhost:{TesterConstants.TesterPort}/metrics");
-            httpRequest.Method = "GET";
-
-            using (var httpResponse = (HttpWebResponse)httpRequest.GetResponse())
-            {
-                var text = new StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
-                Console.WriteLine(text);
-            }
-        }
-
-        private void StartDummyRequest()
-        {
-            Task.Run(delegate
-            {
-                var httpRequest = (HttpWebRequest)WebRequest.Create($"http://localhost:{TesterConstants.TesterPort}/api/Dummy");
-                httpRequest.Method = "GET";
-
-                httpRequest.GetResponse().Dispose();
-            });
-        }
-
-        public override void OnEnd()
-        {
-            _cts.Cancel();
-
-            try
-            {
-                _webserverTask.GetAwaiter().GetResult();
-            }
-            catch (OperationCanceledException)
-            {
-            }
-        }
-
-        public override IMetricServer InitializeMetricServer() => null;
     }
 }
