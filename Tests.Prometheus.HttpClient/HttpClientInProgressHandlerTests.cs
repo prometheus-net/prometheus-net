6a2684d4a3e5b71d2456dab9abb295060ec3f229
ahmad <achehre@yahoo.com>
2020-07-14 18:14:14 +0430

    test: write test for InProgress handler in HttpClient

---
+++ Tests.Prometheus.HttpClient/HttpClientInProgressHandlerTests.cs
@@ -0,0 +1,76 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prometheus;
+using Prometheus.HttpClientMetrics;
+
+namespace Tests.Prometheus.HttpClient
+{
+    [TestClass]
+    public class HttpClientInProgressHandlerTests
+    {
+        [TestMethod]
+        public async Task when_i_request_a_uri_should_inprogress_metric_added_by_one()
+        {
+            //////////////////////////////////////
+            // Arrange
+            //////////////////////////////////////
+
+            var gauge = Metrics.CreateGauge(
+                                            "httpclient_requests_in_progress",
+                                            "The number of requests currently in progress in the HttpClient pipeline.",
+                                            new GaugeConfiguration
+                                            {
+                                                LabelNames = HttpClientRequestLabelNames.All
+                                            });
+
+
+            var captureGaugeValueHttpHandler = new CaptureGaugeValueHttpHandler();
+            captureGaugeValueHttpHandler.Gauge = gauge;
+
+
+            var options = new HttpClientInProgressOptions();
+            options.Gauge = gauge;
+
+            var httpClientInProgressHandler =
+                new HttpClientInProgressHandler(captureGaugeValueHttpHandler, options);
+
+
+            var httpClient = new System.Net.Http.HttpClient(httpClientInProgressHandler);
+
+
+            //////////////////////////////////////
+            // Act
+            //////////////////////////////////////
+
+            await httpClient.GetAsync("http://www.google.com").ConfigureAwait(false);
+
+            //////////////////////////////////////
+            // Assert
+            //////////////////////////////////////
+
+
+            Assert.AreEqual(1, captureGaugeValueHttpHandler.CapturedValue);
+        }
+
+        internal class CaptureGaugeValueHttpHandler : DelegatingHandler
+        {
+            public CaptureGaugeValueHttpHandler()
+            {
+                InnerHandler = new HttpClientHandler();
+            }
+
+            public Gauge Gauge { get; set; }
+            public double CapturedValue { get; set; }
+
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                                                                   CancellationToken cancellationToken)
+            {
+                CapturedValue = Gauge.WithLabels("GET", "www.google.com", "/").Value;
+                return base.SendAsync(request, cancellationToken);
+            }
+        }
+    }
+}
