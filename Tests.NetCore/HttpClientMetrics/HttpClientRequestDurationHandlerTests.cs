c0843e3c857544a7c0a93c59f6a1a7697e0d951d
Kenneth Kam <kenkam@gmail.com>
2020-11-01 14:21:45 +0000

    Await SendAsync to record correct duration

--- Tests.NetCore/HttpClientMetrics/HttpClientRequestDurationHandlerTests.cs
+++ Tests.NetCore/HttpClientMetrics/HttpClientRequestDurationHandlerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Prometheus.HttpClientMetrics;
@@ -28,6 +29,52 @@ namespace Prometheus.Tests.HttpClientMetrics
 
             Assert.AreEqual(1, handler._metric.WithLabels("GET", "www.google.com").Count);
             Assert.IsTrue(handler._metric.WithLabels("GET", "www.google.com").Sum > 0);
+        }
+        
+        [TestMethod]
+        public void OnRequest_AwaitsRequestAndRecordsDuration()
+        {
+            var registry = Metrics.NewCustomRegistry();
+
+            var options = new HttpClientRequestDurationOptions
+            {
+                Registry = registry
+            };
+
+            var handler = new HttpClientRequestDurationHandler(options);
+
+            // Use a mock client handler so we can control when the task completes
+            var mockHttpClientHandler = new MockHttpClientHandler();
+            handler.InnerHandler = mockHttpClientHandler;
+
+            var client = new HttpClient(handler);
+            client.GetAsync("http://www.google.com");
+
+            // There should be no duration metric recorded unless the task is completed
+            Assert.AreEqual(0, handler._metric.WithLabels("GET", "www.google.com").Count);
+            
+            mockHttpClientHandler.Complete();
+            Assert.AreEqual(1, handler._metric.WithLabels("GET", "www.google.com").Count);
+        }
+
+        private class MockHttpClientHandler : HttpClientHandler
+        {
+            private readonly TaskCompletionSource<HttpResponseMessage> _taskCompletionSource;
+
+            public MockHttpClientHandler()
+            {
+                _taskCompletionSource = new TaskCompletionSource<HttpResponseMessage>();
+            }
+
+            public void Complete()
+            {
+                _taskCompletionSource.SetResult(new HttpResponseMessage());
+            }
+            
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                return _taskCompletionSource.Task;
+            }
         }
     }
 }
