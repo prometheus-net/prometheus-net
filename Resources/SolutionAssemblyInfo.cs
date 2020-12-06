15be8f3a143a33da4cbd8c6f33c20c56939558ae
Sander Saares <sasaares@microsoft.com>
2020-12-06 19:25:02 +0200

    4.1.0

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 // This is the real version number, used in NuGet packages and for display purposes.
-[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.1.0")]
 
 // Only use major version here, with others kept at zero, for correct assembly binding logic.
 [assembly: AssemblyVersion("4.0.0")]
