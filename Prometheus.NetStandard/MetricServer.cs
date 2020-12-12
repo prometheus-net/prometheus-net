39d724ee739bba313f0cd901c3855c5e24682444
Sander Saares <sasaares@microsoft.com>
2020-12-12 13:09:05 +0200

    Added request filtering (authorization) support to standalone MetricServer.
    Closes #261

--- Prometheus.NetStandard/MetricServer.cs
+++ Prometheus.NetStandard/MetricServer.cs
@@ -15,6 +15,12 @@ namespace Prometheus
     {
         private readonly HttpListener _httpListener = new HttpListener();
 
+        /// <summary>
+        /// Only requests that match this predicate will be served by the metric server. This allows you to add authorization checks.
+        /// By default (if null), all requests are served.
+        /// </summary>
+        public Func<HttpListenerRequest, bool>? RequestPredicate { get; set; }
+
         public MetricServer(int port, string url = "metrics/", CollectorRegistry? registry = null, bool useHttps = false) : this("+", port, url, registry, useHttps)
         {
         }
@@ -36,6 +42,7 @@ namespace Prometheus
                 try
                 {
                     Thread.CurrentThread.Name = "Metric Server";     //Max length 16 chars (Linux limitation)
+
                     while (!cancel.IsCancellationRequested)
                     {
                         // There is no way to give a CancellationToken to GCA() so, we need to hack around it a bit.
@@ -45,62 +52,69 @@ namespace Prometheus
 
                         // Asynchronously process the request.
                         _ = Task.Factory.StartNew(async delegate
-                          {
-                              var request = context.Request;
-                              var response = context.Response;
+                        {
+                            var request = context.Request;
+                            var response = context.Response;
 
-                              try
-                              {
-                                  Thread.CurrentThread.Name = "Metric Process";
-                                  try
-                                  {
-                                      // We first touch the response.OutputStream only in the callback because touching
-                                      // it means we can no longer send headers (the status code).
-                                      var serializer = new TextSerializer(delegate
-                                        {
-                                            response.ContentType = PrometheusConstants.ExporterContentType;
-                                            response.StatusCode = 200;
-                                            return response.OutputStream;
-                                        });
+                            try
+                            {
+                                var predicate = RequestPredicate;
 
-                                      await _registry.CollectAndSerializeAsync(serializer, cancel);
-                                      response.OutputStream.Dispose();
-                                  }
-                                  catch (ScrapeFailedException ex)
-                                  {
-                                      // This can only happen before anything is written to the stream, so it
-                                      // should still be safe to update the status code and report an error.
-                                      response.StatusCode = 503;
+                                if (predicate != null && !predicate(request))
+                                {
+                                    // Request rejected by predicate.
+                                    response.StatusCode = (int)HttpStatusCode.Forbidden;
+                                    return;
+                                }
 
-                                      if (!string.IsNullOrWhiteSpace(ex.Message))
-                                      {
-                                          using (var writer = new StreamWriter(response.OutputStream))
-                                              writer.Write(ex.Message);
-                                      }
-                                  }
-                              }
-                              catch (Exception ex) when (!(ex is OperationCanceledException))
-                              {
-                                  if (!_httpListener.IsListening)
-                                      return; // We were shut down.
+                                try
+                                {
+                                    // We first touch the response.OutputStream only in the callback because touching
+                                    // it means we can no longer send headers (the status code).
+                                    var serializer = new TextSerializer(delegate
+                                    {
+                                        response.ContentType = PrometheusConstants.ExporterContentType;
+                                        response.StatusCode = 200;
+                                        return response.OutputStream;
+                                    });
 
-                                  Trace.WriteLine(string.Format("Error in {0}: {1}", nameof(MetricServer), ex));
+                                    await _registry.CollectAndSerializeAsync(serializer, cancel);
+                                    response.OutputStream.Dispose();
+                                }
+                                catch (ScrapeFailedException ex)
+                                {
+                                    // This can only happen before anything is written to the stream, so it
+                                    // should still be safe to update the status code and report an error.
+                                    response.StatusCode = 503;
 
-                                  try
-                                  {
-                                      response.StatusCode = 500;
-                                  }
-                                  catch
-                                  {
-                                      // Might be too late in request processing to set response code, so just ignore.
-                                  }
-                              }
-                              finally
-                              {
-                                  response.Close();
-                              }
-                          });
+                                    if (!string.IsNullOrWhiteSpace(ex.Message))
+                                    {
+                                        using (var writer = new StreamWriter(response.OutputStream))
+                                            writer.Write(ex.Message);
+                                    }
+                                }
+                            }
+                            catch (Exception ex) when (!(ex is OperationCanceledException))
+                            {
+                                if (!_httpListener.IsListening)
+                                    return; // We were shut down.
 
+                                Trace.WriteLine(string.Format("Error in {0}: {1}", nameof(MetricServer), ex));
+
+                                try
+                                {
+                                    response.StatusCode = 500;
+                                }
+                                catch
+                                {
+                                    // Might be too late in request processing to set response code, so just ignore.
+                                }
+                            }
+                            finally
+                            {
+                                response.Close();
+                            }
+                        });
                     }
                 }
                 finally
