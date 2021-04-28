ca9545462893940af0a5f4497410a7386c093bbe
Rune Rystad <rune.rystad@elvia.no>
2021-04-28 14:04:21 +0200

    Handle HttpClient timeout to avoid terminating push thread

--- Tests.NetFramework/MetricPusherTests.cs
+++ Tests.NetFramework/MetricPusherTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Prometheus.Tests
 {
@@ -36,6 +38,69 @@ namespace Prometheus.Tests
             Assert.IsNotNull(lastError);
 
             pusher.Stop();
+        }
+
+        [TestMethod]
+        public void HttpClient_OnInvalidRequestUri_CallsErrorCallback()
+        {
+            RunHttpClientExceptionScenario(new InvalidOperationException("Simulating an invalid request uri according to HttpClient documentation."));
+        }
+
+        [TestMethod]
+        public void HttpClient_OnUnderlyingConnectionIssue_CallsErrorCallback()
+        {
+            RunHttpClientExceptionScenario(new HttpRequestException("Simulating an underlying connection issue according to HttpClient documentation."));
+        }
+
+        [TestMethod]
+        public void HttpClient_OnTimeout_CallsErrorCallback()
+        {
+            RunHttpClientExceptionScenario(new TaskCanceledException("Simulating a timeout according to HttpClient documentation."));
+        }
+
+        private void RunHttpClientExceptionScenario(Exception throwOnHttpPost)
+        {
+            Exception lastError = null;
+            var onErrorCalled = new ManualResetEventSlim();
+
+            void OnError(Exception ex)
+            {
+                lastError = ex;
+                onErrorCalled.Set();
+            }
+
+            var pusher = new MetricPusher(new MetricPusherOptions
+            {
+                Job = "Test",
+                // Small interval to ensure that we exit fast.
+                IntervalMilliseconds = 100,
+                Endpoint = "https://any_valid.url/the_push_fails_with_fake_httpclient_throwing_exceptions",
+                OnError = OnError,
+                HttpClientProvider = () => new ThrowingHttpClient(throwOnHttpPost)
+            });
+
+            pusher.Start();
+
+            var onErrorWasCalled = onErrorCalled.Wait(TimeSpan.FromSeconds(5));
+            Assert.IsTrue(onErrorWasCalled, "OnError was not called even though the push failed.");
+            Assert.IsNotNull(lastError);
+
+            pusher.Stop();
+        }
+
+        private class ThrowingHttpClient : HttpClient
+        {
+            private readonly Exception _throwOnSend;
+            public ThrowingHttpClient(Exception ex)
+            {
+                _throwOnSend = ex;
+            }
+            
+            // PostAsync eventually calls SendAsync
+            public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                throw _throwOnSend;
+            }
         }
     }
 }
