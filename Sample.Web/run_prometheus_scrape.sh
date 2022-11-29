c27f3c60318554f4a2af894090fc91e117057698
Hassan Syed <hassan.syed@paymentsense.com>
2022-11-29 12:29:39 +0000

    Add a helper script to launch prometheus for testing purposes.

---
+++ Sample.Web/run_prometheus_scrape.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+
+# Runs a prometheus scrape on an endpoint with exemplar storage enabled. localhost:9090 will take you to the prometheus 
+# ui for verification.
+
+cat <<EOF > prometheus.yml
+scrape_configs:
+  - job_name: 'prometheus-net'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['$1']
+EOF
+
+docker run --rm -it --name prometheus -p 9090:9090 \
+    -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
+    prom/prometheus \
+    --config.file=/etc/prometheus/prometheus.yml \
+    --log.level=debug \
+    --enable-feature=exemplar-storage
