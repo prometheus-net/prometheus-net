67859724b717f491100faff5c02628b3ec3c0c56
Sander Saares <sasaares@microsoft.com>
2022-12-29 10:23:24 +0200

    Enable exemplars for LocalMetricsCollector

--- LocalMetricsCollector/run.sh
+++ LocalMetricsCollector/run.sh
@@ -6,4 +6,4 @@ set -e
 envsubst < /app/prometheus.yml > /etc/prometheus/prometheus.yml
 
 # We must listen on 0.0.0.0 here because otherwise the liveness/readiness probes cannot reach us.
-exec /bin/prometheus --web.listen-address=0.0.0.0:$PROMETHEUS_PORT --storage.tsdb.retention.time=5m --storage.tsdb.min-block-duration=2m --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
+exec /bin/prometheus --web.listen-address=0.0.0.0:$PROMETHEUS_PORT --storage.tsdb.retention.time=5m --storage.tsdb.min-block-duration=2m --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --enable-feature=exemplar-storage
