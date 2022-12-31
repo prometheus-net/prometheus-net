fb39dcb52e2539acb83547929d3691fabdced726
Sander Saares <sasaares@microsoft.com>
2022-12-31 08:47:28 +0200

    Merge nuspec files into csproj

--- Prometheus.NetFramework.AspNet/AspNetMetricServer.cs
+++ Prometheus.NetFramework.AspNet/AspNetMetricServer.cs
@@ -1,78 +1,73 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net;
 using System.Web.Http;
 
-namespace Prometheus
+namespace Prometheus;
+
+public static class AspNetMetricServer
 {
-    public static class AspNetMetricServer
+    private const string RouteNamePrefix = "Prometheus_";
+
+    public sealed class Settings
     {
-        private const string RouteNamePrefix = "Prometheus_";
+        public CollectorRegistry? Registry { get; set; }
+    }
 
-        public sealed class Settings
+    /// <summary>
+    /// Registers an anonymous instance of the controller to be published on the /metrics URL.
+    /// </summary>
+    public static void RegisterRoutes(HttpConfiguration configuration, Settings? settings = null) =>
+        MapRoute(configuration, "Default", "metrics", settings);
+
+    /// <summary>
+    /// Registers an anonymous instance of the controller to be published on a given URL path (e.g. "custom/metrics").
+    /// </summary>
+    public static void RegisterRoutes(HttpConfiguration configuration, string path, Settings? settings = null) =>
+        MapRoute(configuration, path, path, settings);
+
+    private static void MapRoute(HttpConfiguration configuration, string routeName, string routeTemplate, Settings? settings)
+    {
+        if (configuration == null)
+            throw new ArgumentNullException(nameof(configuration));
+
+        configuration.Routes.MapHttpRoute(
+            name: $"{RouteNamePrefix}{routeName}",
+            routeTemplate: routeTemplate,
+            defaults: null,
+            constraints: null,
+            handler: new Handler(settings?.Registry ?? Metrics.DefaultRegistry));
+    }
+
+    private sealed class Handler : HttpMessageHandler
+    {
+        public Handler(CollectorRegistry registry)
         {
-            public CollectorRegistry Registry { get; set; }
+            _registry = registry;
         }
 
-        /// <summary>
-        /// Registers an anonymous instance of the controller to be published on the /metrics URL.
-        /// </summary>
-        public static void RegisterRoutes(HttpConfiguration configuration, Settings settings = null) =>
-            MapRoute(configuration, "Default", "metrics", settings);
+        private readonly CollectorRegistry _registry;
 
-        /// <summary>
-        /// Registers an anonymous instance of the controller to be published on a given URL path (e.g. "custom/metrics").
-        /// </summary>
-        public static void RegisterRoutes(HttpConfiguration configuration, string path, Settings settings = null) =>
-            MapRoute(configuration, path, path, settings);
-
-        private static void MapRoute(HttpConfiguration configuration, string routeName, string routeTemplate, Settings settings)
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            if (configuration == null)
-                throw new ArgumentNullException(nameof(configuration));
+            // The ASP.NET PushStreamContent does not have a way to easily handle exceptions that
+            // occur before we write to the stream (when we can still return nice error headers).
+            // Maybe in a future version this could be improved, as right now exception == aborted connection.
 
-            configuration.Routes.MapHttpRoute(
-                name: $"{RouteNamePrefix}{routeName}",
-                routeTemplate: routeTemplate,
-                defaults: null,
-                constraints: null,
-                handler: new Handler(settings?.Registry ?? Metrics.DefaultRegistry));
-        }
-
-        private sealed class Handler : HttpMessageHandler
-        {
-            public Handler(CollectorRegistry registry)
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                _registry = registry;
-            }
-
-            private readonly CollectorRegistry _registry;
-
-            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-            {
-                // The ASP.NET PushStreamContent does not have a way to easily handle exceptions that
-                // occur before we write to the stream (when we can still return nice error headers).
-                // Maybe in a future version this could be improved, as right now exception == aborted connection.
-
-                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                Content = new PushStreamContent(async (stream, content, context) =>
                 {
-                    Content = new PushStreamContent(async (stream, content, context) =>
+                    try
                     {
-                        try
-                        {
-                            await _registry.CollectAndExportAsTextAsync(stream, ExpositionFormat.Text, cancellationToken);
-                        }
-                        finally
-                        {
-                            stream.Close();
-                        }
-                    }, PrometheusConstants.ExporterContentTypeValue)
-                };
+                        await _registry.CollectAndExportAsTextAsync(stream, ExpositionFormat.Text, cancellationToken);
+                    }
+                    finally
+                    {
+                        stream.Close();
+                    }
+                }, PrometheusConstants.ExporterContentTypeValue)
+            };
 
-                return Task.FromResult(response);
-            }
+            return Task.FromResult(response);
         }
     }
 }
