153cb23158f8a891d8675e0248df95f1916a0011
Sander Saares <sasaares@microsoft.com>
2023-01-03 06:40:22 +0200

    Reduce memory allocations performed during ASP.NET Core HTTP request tracking

--- Benchmark.NetCore/HttpExporterBenchmarks.cs
+++ Benchmark.NetCore/HttpExporterBenchmarks.cs
@@ -3,59 +3,61 @@ using Microsoft.AspNetCore.Http;
 using Prometheus;
 using Prometheus.HttpMetrics;
 
-namespace Benchmark.NetCore
+namespace Benchmark.NetCore;
+
+[MemoryDiagnoser]
+public class HttpExporterBenchmarks
 {
-    [MemoryDiagnoser]
-    public class HttpExporterBenchmarks
+    private CollectorRegistry _registry;
+    private MetricFactory _factory;
+    private HttpInProgressMiddleware _inProgressMiddleware;
+    private HttpRequestCountMiddleware _countMiddleware;
+    private HttpRequestDurationMiddleware _durationMiddleware;
+
+    [Params(100_000)]
+    public int RequestCount { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
     {
-        private CollectorRegistry _registry;
-        private MetricFactory _factory;
-        private HttpInProgressMiddleware _inProgressMiddleware;
-        private HttpRequestCountMiddleware _countMiddleware;
-        private HttpRequestDurationMiddleware _durationMiddleware;
+        _registry = Metrics.NewCustomRegistry();
+        _factory = Metrics.WithCustomRegistry(_registry);
 
-        [Params(1000, 10000)]
-        public int RequestCount { get; set; }
-
-        [GlobalSetup]
-        public void Setup()
+        _inProgressMiddleware = new HttpInProgressMiddleware(next => Task.CompletedTask, new HttpInProgressOptions
         {
-            _registry = Metrics.NewCustomRegistry();
-            _factory = Metrics.WithCustomRegistry(_registry);
-
-            _inProgressMiddleware = new HttpInProgressMiddleware(next => Task.CompletedTask, new HttpInProgressOptions
-            {
-                Gauge = _factory.CreateGauge("in_progress", "help")
-            });
-            _countMiddleware = new HttpRequestCountMiddleware(next => Task.CompletedTask, new HttpRequestCountOptions
-            {
-                Counter = _factory.CreateCounter("count", "help")
-            });
-            _durationMiddleware = new HttpRequestDurationMiddleware(next => Task.CompletedTask, new HttpRequestDurationOptions
-            {
-                Histogram = _factory.CreateHistogram("duration", "help")
-            });
-        }
-
-        [Benchmark]
-        public async Task HttpInProgress()
+            Gauge = _factory.CreateGauge("in_progress", "help")
+        });
+        _countMiddleware = new HttpRequestCountMiddleware(next => Task.CompletedTask, new HttpRequestCountOptions
         {
-            for (var i = 0; i < RequestCount; i++)
-                await _inProgressMiddleware.Invoke(new DefaultHttpContext());
-        }
-
-        [Benchmark]
-        public async Task HttpRequestCount()
+            Counter = _factory.CreateCounter("count", "help")
+        });
+        _durationMiddleware = new HttpRequestDurationMiddleware(next => Task.CompletedTask, new HttpRequestDurationOptions
         {
-            for (var i = 0; i < RequestCount; i++)
-                await _countMiddleware.Invoke(new DefaultHttpContext());
-        }
+            Histogram = _factory.CreateHistogram("duration", "help")
+        });
+    }
 
-        [Benchmark]
-        public async Task HttpRequestDuration()
-        {
-            for (var i = 0; i < RequestCount; i++)
-                await _durationMiddleware.Invoke(new DefaultHttpContext());
-        }
+    // Reuse the same HttpContext for different requests, to not count its overhead in the benchmark.
+    private static readonly DefaultHttpContext _httpContext = new();
+
+    [Benchmark]
+    public async Task HttpInProgress()
+    {
+        for (var i = 0; i < RequestCount; i++)
+            await _inProgressMiddleware.Invoke(_httpContext);
+    }
+
+    [Benchmark]
+    public async Task HttpRequestCount()
+    {
+        for (var i = 0; i < RequestCount; i++)
+            await _countMiddleware.Invoke(_httpContext);
+    }
+
+    [Benchmark]
+    public async Task HttpRequestDuration()
+    {
+        for (var i = 0; i < RequestCount; i++)
+            await _durationMiddleware.Invoke(_httpContext);
     }
 }
--- Prometheus/GaugeExtensions.cs
+++ Prometheus/GaugeExtensions.cs
@@ -1,76 +1,94 @@
-﻿namespace Prometheus
+﻿using Microsoft.Extensions.ObjectPool;
+
+namespace Prometheus;
+
+public static class GaugeExtensions
 {
-    public static class GaugeExtensions
+    /// <summary>
+    /// Sets the value of the gauge to the current UTC time as a Unix timestamp in seconds.
+    /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
+    /// </summary>
+    public static void SetToCurrentTimeUtc(this IGauge gauge)
     {
-        /// <summary>
-        /// Sets the value of the gauge to the current UTC time as a Unix timestamp in seconds.
-        /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
-        /// </summary>
-        public static void SetToCurrentTimeUtc(this IGauge gauge)
+        gauge.Set(TimestampHelpers.ToUnixTimeSecondsAsDouble(DateTimeOffset.UtcNow));
+    }
+
+    /// <summary>
+    /// Sets the value of the gauge to a specific moment as the UTC timezone Unix timestamp in seconds.
+    /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
+    /// </summary>
+    public static void SetToTimeUtc(this IGauge gauge, DateTimeOffset timestamp)
+    {
+        gauge.Set(TimestampHelpers.ToUnixTimeSecondsAsDouble(timestamp));
+    }
+
+    /// <summary>
+    /// Increments the value of the gauge to the current UTC time as a Unix timestamp in seconds.
+    /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
+    /// Operation is ignored if the current value is already greater.
+    /// </summary>
+    public static void IncToCurrentTimeUtc(this IGauge gauge)
+    {
+        gauge.IncTo(TimestampHelpers.ToUnixTimeSecondsAsDouble(DateTimeOffset.UtcNow));
+    }
+
+    /// <summary>
+    /// Increments the value of the gauge to a specific moment as the UTC Unix timestamp in seconds.
+    /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
+    /// Operation is ignored if the current value is already greater.
+    /// </summary>
+    public static void IncToTimeUtc(this IGauge gauge, DateTimeOffset timestamp)
+    {
+        gauge.IncTo(TimestampHelpers.ToUnixTimeSecondsAsDouble(timestamp));
+    }
+
+    private sealed class InProgressTracker : IDisposable
+    {
+        public void Dispose()
         {
-            gauge.Set(TimestampHelpers.ToUnixTimeSecondsAsDouble(DateTimeOffset.UtcNow));
+            if (_gauge == null)
+                return;
+
+            _gauge.Dec();
+            _gauge = null;
+            Pool.Return(this);
         }
 
-        /// <summary>
-        /// Sets the value of the gauge to a specific moment as the UTC timezone Unix timestamp in seconds.
-        /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
-        /// </summary>
-        public static void SetToTimeUtc(this IGauge gauge, DateTimeOffset timestamp)
+        private IGauge? _gauge;
+
+        public void Update(IGauge gauge)
         {
-            gauge.Set(TimestampHelpers.ToUnixTimeSecondsAsDouble(timestamp));
+            if (_gauge != null)
+                throw new InvalidOperationException($"{nameof(InProgressTracker)} was reused before being disposed.");
+
+            _gauge = gauge;
         }
 
-        /// <summary>
-        /// Increments the value of the gauge to the current UTC time as a Unix timestamp in seconds.
-        /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
-        /// Operation is ignored if the current value is already greater.
-        /// </summary>
-        public static void IncToCurrentTimeUtc(this IGauge gauge)
+        public static InProgressTracker Create(IGauge gauge)
         {
-            gauge.IncTo(TimestampHelpers.ToUnixTimeSecondsAsDouble(DateTimeOffset.UtcNow));
+            var instance = Pool.Get();
+            instance.Update(gauge);
+            return instance;
         }
 
-        /// <summary>
-        /// Increments the value of the gauge to a specific moment as the UTC Unix timestamp in seconds.
-        /// Value does not include any elapsed leap seconds because Unix timestamps do not include leap seconds.
-        /// Operation is ignored if the current value is already greater.
-        /// </summary>
-        public static void IncToTimeUtc(this IGauge gauge, DateTimeOffset timestamp)
-        {
-            gauge.IncTo(TimestampHelpers.ToUnixTimeSecondsAsDouble(timestamp));
-        }
+        private static readonly ObjectPool<InProgressTracker> Pool = ObjectPool.Create<InProgressTracker>();
+    }
 
-        private sealed class InProgressTracker : IDisposable
-        {
-            public InProgressTracker(IGauge gauge)
-            {
-                _gauge = gauge;
-            }
+    /// <summary>
+    /// Tracks the number of in-progress operations taking place.
+    /// 
+    /// Calling this increments the gauge. Disposing of the returned instance decrements it again.
+    /// </summary>
+    /// <remarks>
+    /// It is safe to track the sum of multiple concurrent in-progress operations with the same gauge.
+    /// </remarks>
+    public static IDisposable TrackInProgress(this IGauge gauge)
+    {
+        if (gauge == null)
+            throw new ArgumentNullException(nameof(gauge));
 
-            public void Dispose()
-            {
-                _gauge.Dec();
-            }
+        gauge.Inc();
 
-            private readonly IGauge _gauge;
-        }
-
-        /// <summary>
-        /// Tracks the number of in-progress operations taking place.
-        /// 
-        /// Calling this increments the gauge. Disposing of the returned instance decrements it again.
-        /// </summary>
-        /// <remarks>
-        /// It is safe to track the sum of multiple concurrent in-progress operations with the same gauge.
-        /// </remarks>
-        public static IDisposable TrackInProgress(this IGauge gauge)
-        {
-            if (gauge == null)
-                throw new ArgumentNullException(nameof(gauge));
-
-            gauge.Inc();
-
-            return new InProgressTracker(gauge);
-        }
+        return InProgressTracker.Create(gauge);
     }
 }
