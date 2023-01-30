ba6cb14ae70da7e6880642bd7148549c557b4600
Sander Saares <sasaares@microsoft.com>
2023-01-30 08:47:08 +0200

    Add HttpMiddlewareExporterOptions.SetMetricFactory

--- Prometheus.AspNetCore/HttpMetrics/HttpMiddlewareExporterOptions.cs
+++ Prometheus.AspNetCore/HttpMetrics/HttpMiddlewareExporterOptions.cs
@@ -1,65 +1,75 @@
 using Microsoft.AspNetCore.Http;
 
-namespace Prometheus.HttpMetrics
+namespace Prometheus.HttpMetrics;
+
+public sealed class HttpMiddlewareExporterOptions
 {
-    public sealed class HttpMiddlewareExporterOptions
+    public HttpInProgressOptions InProgress { get; set; } = new HttpInProgressOptions();
+    public HttpRequestCountOptions RequestCount { get; set; } = new HttpRequestCountOptions();
+    public HttpRequestDurationOptions RequestDuration { get; set; } = new HttpRequestDurationOptions();
+
+    /// <summary>
+    /// Whether to capture metrics for queries to the /metrics endpoint (where metrics are exported by default). Defaults to false.
+    /// This matches against URLs starting with the /metrics string specifically - if you use a custom metrics endpoint, this will not match.
+    /// </summary>
+    public bool CaptureMetricsUrl { get; set; }
+
+    /// <summary>
+    /// Configures all the different types of metrics to use reduced status code cardinality (using 2xx instead of 200, 201 etc).
+    /// </summary>
+    public void ReduceStatusCodeCardinality()
     {
-        public HttpInProgressOptions InProgress { get; set; } = new HttpInProgressOptions();
-        public HttpRequestCountOptions RequestCount { get; set; } = new HttpRequestCountOptions();
-        public HttpRequestDurationOptions RequestDuration { get; set; } = new HttpRequestDurationOptions();
+        InProgress.ReduceStatusCodeCardinality = true;
+        RequestCount.ReduceStatusCodeCardinality = true;
+        RequestDuration.ReduceStatusCodeCardinality = true;
+    }
 
-        /// <summary>
-        /// Whether to capture metrics for queries to the /metrics endpoint (where metrics are exported by default). Defaults to false.
-        /// This matches against URLs starting with the /metrics string specifically - if you use a custom metrics endpoint, this will not match.
-        /// </summary>
-        public bool CaptureMetricsUrl { get; set; }
+    /// <summary>
+    /// Adds an additional route parameter to all the HTTP metrics.
+    /// 
+    /// Helper method to avoid manually adding it to each one.
+    /// </summary>
+    public void AddRouteParameter(HttpRouteParameterMapping mapping)
+    {
+        InProgress.AdditionalRouteParameters.Add(mapping);
+        RequestCount.AdditionalRouteParameters.Add(mapping);
+        RequestDuration.AdditionalRouteParameters.Add(mapping);
+    }
 
-        /// <summary>
-        /// Configures all the different types of metrics to use reduced status code cardinality (using 2xx instead of 200, 201 etc).
-        /// </summary>
-        public void ReduceStatusCodeCardinality()
-        {
-            InProgress.ReduceStatusCodeCardinality = true;
-            RequestCount.ReduceStatusCodeCardinality = true;
-            RequestDuration.ReduceStatusCodeCardinality = true;
-        }
+    /// <summary>
+    /// Adds a custom label to all the HTTP metrics.
+    /// 
+    /// Helper method to avoid manually adding it to each one.
+    /// </summary>
+    public void AddCustomLabel(HttpCustomLabel mapping)
+    {
+        InProgress.CustomLabels.Add(mapping);
+        RequestCount.CustomLabels.Add(mapping);
+        RequestDuration.CustomLabels.Add(mapping);
+    }
 
-        /// <summary>
-        /// Adds an additional route parameter to all the HTTP metrics.
-        /// 
-        /// Helper method to avoid manually adding it to each one.
-        /// </summary>
-        public void AddRouteParameter(HttpRouteParameterMapping mapping)
-        {
-            InProgress.AdditionalRouteParameters.Add(mapping);
-            RequestCount.AdditionalRouteParameters.Add(mapping);
-            RequestDuration.AdditionalRouteParameters.Add(mapping);
-        }
+    /// <summary>
+    /// Adds a custom label to all the HTTP metrics.
+    /// 
+    /// Helper method to avoid manually adding it to each one.
+    /// </summary>
+    public void AddCustomLabel(string labelName, Func<HttpContext, string> valueProvider)
+    {
+        var mapping = new HttpCustomLabel(labelName, valueProvider);
 
-        /// <summary>
-        /// Adds a custom label to all the HTTP metrics.
-        /// 
-        /// Helper method to avoid manually adding it to each one.
-        /// </summary>
-        public void AddCustomLabel(HttpCustomLabel mapping)
-        {
-            InProgress.CustomLabels.Add(mapping);
-            RequestCount.CustomLabels.Add(mapping);
-            RequestDuration.CustomLabels.Add(mapping);
-        }
+        InProgress.CustomLabels.Add(mapping);
+        RequestCount.CustomLabels.Add(mapping);
+        RequestDuration.CustomLabels.Add(mapping);
+    }
 
-        /// <summary>
-        /// Adds a custom label to all the HTTP metrics.
-        /// 
-        /// Helper method to avoid manually adding it to each one.
-        /// </summary>
-        public void AddCustomLabel(string labelName, Func<HttpContext, string> valueProvider)
-        {
-            var mapping = new HttpCustomLabel(labelName, valueProvider);
-
-            InProgress.CustomLabels.Add(mapping);
-            RequestCount.CustomLabels.Add(mapping);
-            RequestDuration.CustomLabels.Add(mapping);
-        }
+    /// <summary>
+    /// Configures the middleware to use a custom metric factory for creating the metrics.
+    /// This provides an easy way to add custom static labels and configure exemplar behavior for all created metrics.
+    /// </summary>
+    public void SetMetricFactory(IMetricFactory metricFactory)
+    {
+        InProgress.MetricFactory = metricFactory;
+        RequestCount.MetricFactory = metricFactory;
+        RequestDuration.MetricFactory = metricFactory;
     }
 }
--- README.md
+++ README.md
@@ -828,7 +828,7 @@ The level of detail obtained from this is rather low - only the total count for 
 
 You can configure the integration using `Metrics.ConfigureEventCounterAdapter()`.
 
-By default, prometheus-net will only publish a small predefined set of general-purpose useful event counters to minimize resource consumption in the default configuration. A custom event source filter must now be provided in the configuration to enable publishing of additional event counters.
+By default, prometheus-net will only publish [the well-known .NET EventCounters](https://learn.microsoft.com/en-us/dotnet/core/diagnostics/available-counters) to minimize resource consumption in the default configuration. A custom event source filter must be provided in the configuration to enable publishing of additional event counters.
 
 See also, [Sample.Console](Sample.Console/Program.cs).
 
