e91dace42e446ca9d19c8582a7e5c2f2b02747c3
Sander Saares <sasaares@microsoft.com>
2023-12-03 13:44:25 +0200

    Code tidy

--- Prometheus/EventCounterAdapterEventSourceSettings.cs
+++ Prometheus/EventCounterAdapterEventSourceSettings.cs
@@ -1,20 +1,19 @@
 ﻿using System.Diagnostics.Tracing;
 
-namespace Prometheus
+namespace Prometheus;
+
+/// <summary>
+/// Defines how the EventCounterAdapter will subscribe to an event source.
+/// </summary>
+public sealed class EventCounterAdapterEventSourceSettings
 {
     /// <summary>
-    /// Defines how the EventCounterAdapter will subscribe to an event source.
+    /// Minimum level of events to receive.
     /// </summary>
-    public sealed class EventCounterAdapterEventSourceSettings
-    {
-        /// <summary>
-        /// Minimum level of events to receive.
-        /// </summary>
-        public EventLevel MinimumLevel { get; set; } = EventLevel.Informational;
+    public EventLevel MinimumLevel { get; set; } = EventLevel.Informational;
 
-        /// <summary>
-        /// Event keywords, of which at least one must match for an event to be received.
-        /// </summary>
-        public EventKeywords MatchKeywords { get; set; } = EventKeywords.None;
-    }
+    /// <summary>
+    /// Event keywords, of which at least one must match for an event to be received.
+    /// </summary>
+    public EventKeywords MatchKeywords { get; set; } = EventKeywords.None;
 }
