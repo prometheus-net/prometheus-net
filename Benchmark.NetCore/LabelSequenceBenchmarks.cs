2e9d9265d431141dda7a25e1cffb89a23a8b6b0c
Sander Saares <sasaares@microsoft.com>
2023-12-04 10:10:39 +0200

    StringSequence

---
+++ Benchmark.NetCore/LabelSequenceBenchmarks.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using Prometheus;
+
+namespace Benchmark.NetCore;
+
+[MemoryDiagnoser]
+public class LabelSequenceBenchmarks
+{
+    private static readonly StringSequence Names3Array = StringSequence.From(new[] { "aaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbb", "cccccccccccccc" });
+    private static readonly StringSequence Values3Array = StringSequence.From(new[] { "valueaaaaaaaaaaaaaaaaa", "valuebbbbbbbbbbbbbb", "valuecccccccccccccc" });
+
+    [Benchmark]
+    public void Create_From3Array()
+    {
+        LabelSequence.From(Names3Array, Values3Array);
+    }
+}
---
+++ Benchmark.NetCore/StringSequenceBenchmarks.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+using Prometheus;
+
+namespace Benchmark.NetCore;
+
+[MemoryDiagnoser]
+public class StringSequenceBenchmarks
+{
+    private static readonly string[] Values3Array = ["aaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbb", "cccccccccccccc"];
+
+    [Benchmark]
+    public void Create_From3Array()
+    {
+        StringSequence.From(Values3Array);
+    }
+}
